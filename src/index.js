b923aa1181244e590487bf2616c68f12e2864040
ducuanhaep <tranducanh1012@gmail.com>
2019-08-26 16:59:08 +0700

    finish codesplit [ducanhaep]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
- import _ from 'lodash';
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ "lodash")
+    .then(({ default: _ }) => {
+      const element = document.createElement("div");
 
- function component() {
-   const element = document.createElement('div');
+      element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+      return element;
+    })
+    .catch(error => "An error occurred while loading the component");
+}
 
-   return element;
- }
-
- document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: { index: "./src/index.js" },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
   }
 };
